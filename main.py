caf932da69c28bf8b071c5b91901807552c7f844
MHorn <36743022+MarcelHorn@users.noreply.github.com>
2021-06-11 14:39:34 +0000

    add commentary, refactoring

--- main.py
+++ main.py
@@ -8,27 +8,28 @@ from keep_alive import keep_alive
 
 client = discord.Client()
 
+#Courage quote function
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
   json_data = json.loads(response.text)
   quote = json_data[0]["q"] + " -" + json_data[0]["a"]
   return quote
-
+###
+#joke function
 def get_joke():
   response = requests.get("https://official-joke-api.appspot.com/random_joke")
   json_data = json.loads(response.text)
   joke = json_data["setup"]+ " -" + json_data["punchline"]
   return joke
-
+###
+#Role assign function
 @client.event
 async def on_raw_reaction_add(payload):
   ChID = 852611783696711701 
   guild_id = payload.guild_id
   guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-  
   if payload.channel_id != ChID:
         return
-  
   member = await(await client.fetch_guild(payload.guild_id)).fetch_member(payload.user_id)
   if member is not None:
     if payload.emoji.name == "🏹":
@@ -39,16 +40,15 @@ async def on_raw_reaction_add(payload):
       await member.add_roles(zelda)
   else: 
     print("Member not found")
-
+###
+#Role remove function
 @client.event
 async def on_raw_reaction_remove(payload):
   ChID = 852611783696711701 
   guild_id = payload.guild_id
   guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-  
   if payload.channel_id != ChID:
         return
-  
   member = await(await client.fetch_guild(payload.guild_id)).fetch_member(payload.user_id)
   if member is not None:
     if payload.emoji.name == "🏹":
@@ -59,8 +59,8 @@ async def on_raw_reaction_remove(payload):
       await member.remove_roles(zelda)
   else: 
     print("Member not found")
-
-
+###
+#
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
@@ -70,7 +70,8 @@ async def on_ready():
     #message = await assign_channel.send(Text)
     #await message.add_reaction("🏹")
     #await message.add_reaction('🇿')
-
+###
+#
 @client.event
 async def on_message(message):
   
@@ -79,13 +80,14 @@ async def on_message(message):
   
   member = message.author
   dev_role = discord.utils.get(member.guild.roles, name="Dev Team")
-  #Dev-Commands
+  #bot-dev-Commands
   if message.content.startswith("!id"):
     if dev_role not in member.roles :
       await message.channel.send("You have no permission for this command, sorry.")
     else: 
       await message.channel.send("Channel ID: " + str(message.channel.id) +", User-ID: " + str(member.id))
-
+  ###
+  #bot-commands
   if message.channel.id == 851128177366925332 or message.channel.id == 769815939696033792:    
     if message.content.startswith("!hello"):
       await message.channel.send("Hello!")
@@ -98,6 +100,7 @@ async def on_message(message):
     if message.content.startswith("!joke"):
       joke = get_joke()
       await message.channel.send(joke)
+
 
 token = os.environ['TOKEN']
 
